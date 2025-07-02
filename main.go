5fc9d75f2fe03f5d9bbfbf0313821e7836467ada
RitwikSingh28 <itsritwik26@gmail.com>
2025-07-02 09:19:25 +0530

    feat: init setup

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello there!")
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger.Info("Starting up the Master Process!")
+
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		logger.Error("Failed to listen to socket")
+		panic(err)
+	}
+	defer listener.Close()
+
+	logger.Info("Listening on port 8080")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			logger.Error("Failed to accept connection")
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger.Info("Accepting connection from: ", "address", conn.RemoteAddr().String())
+
+	buffer := make([]byte, 1024)
+	bytes_read, err := conn.Read(buffer)
+	if err != nil {
+		logger.Error("Failed to read request")
+		return
+	}
+	data := buffer[:bytes_read]
+
+	_, err = conn.Write(data)
+	if err != nil {
+		logger.Error("Failed to write response")
+		return
+	}
+
+	logger.Info("Received: ", "data", string(data))
 }
